6a884e0908c3a9f00d63eca169f210b55be4dfa2
Trephlosion <rodneywhitneyiii@gmail.com>
2023-03-23 16:45:38 -0400

    bankACC

---
+++ Chapter6/array_of_obj.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+#include "bank_client/BankAccount.h"
+#include <string>
+
+const int NUM_MACHINE = 3;
+const int NUM_ACCT = 3;
+
+using namespace std;
+
+struct Machine
+{
+    string machine_id;
+    string location;
+
+    Machine(string id, string loc)
+    {
+        location = loc;
+        machine_id = id;
+    }
+};
+
+
+
+
+int main()
+{
+    string answer;
+    string city;
+    int expression;
+    int bank_index;
+    int machine_index;
+
+
+
+    Machine 
+
+
+
+    return 0;
+}//main
