37ddb322270c6b1bf10ddfeab6208b4846e38499
Trephlosion <rodneywhitneyiii@gmail.com>
2023-03-23 16:30:13 -0400

    Chapter 6

---
+++ Chapter6/arrays_example.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    const int NUM_MONTH = 6;
+    string month_name[NUM_MONTH]= {"January", "Febuary", "April", "May", "June", "July"};
+    int day[NUM_MONTH]= {15,28,10,22,17,20};
+
+    // for (int i=0; i<NUM_MONTH; i++)
+    // {
+    //     cout << endl << setw(9) << left << month_name[i] << " - " << day[i] << endl;
+    // }
+
+    string month_day[6][2]= {{"January","15"},{"Febuary","28"},{"April","10"},{"May","22"},{"June","17"},{"July","20"}};
+
+    // for (int j=0; j<NUM_MONTH; j++)
+    // {
+    //     for(int i=0; i<2; i++)
+    //     {
+    //         cout << endl << setw(9) << left << " " << month_day[j][i];
+    //     }
+    //     cout << endl;
+    // }
+
+    for (int j=0; j<NUM_MONTH; j++)
+    {
+        for(int i=1; i>0; i--)
+        {
+            cout << endl << setw(9) << left << " " << month_day[j][i];
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}//main
+
+
+
+
+
